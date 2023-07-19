610598aae04231eaca3351db45a149f7935ecde1
Vivi Ya <viviviyanez@gmail.com>
2023-07-20 01:02:25 +0200

    remove unused font family

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -7,4 +7,9 @@ export default defineConfig({
   description:
     "Hello, welcome to my personal site. Learn more about me, take a look at projects and/or read some of my blog post. Thanks for visiting :)",
   cleanUrls: true,
+  themeConfig: {
+    socialLinks: [
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
 });

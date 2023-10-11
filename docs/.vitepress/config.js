bd02f8c2b833d0a84ec5cac2ef86aafa495d56d7
vivitt <viviviyanez@gmail.com>
2023-10-11 15:46:43 +0200

    updated description, title and title template

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,15 +1,16 @@
-import { defineConfig } from "vitepress";
+import { defineConfig } from 'vitepress';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  lang: "en-US",
-  title: "Viviana Yanez",
-  description:
-    "Hello, welcome to my personal site. Learn more about me, take a look at projects and/or read some of my blog post. Thanks for visiting :)",
+  lang: 'en-US',
+  title: 'Viviana Yanez',
+  titleTemplate: 'Frontend Developer',
+  description: 'I am Viviana Yanez (aka Vivi). I am a frontend developer a visual and audiovisual communication background. I create and build accessible and inclusive user interfaces to provide users with enjoyable web experiences.',
   cleanUrls: true,
   themeConfig: {
     socialLinks: [
-      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' },
     ],
   },
+  appearance: true,
 });

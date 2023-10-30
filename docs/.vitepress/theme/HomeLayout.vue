ff3d33116318cdc4d3321e92d8a626a8122d1af7
vivitt <viviviyanez@gmail.com>
2023-10-30 13:23:52 +0100

    add update logo

--- docs/.vitepress/theme/HomeLayout.vue
+++ docs/.vitepress/theme/HomeLayout.vue
@@ -1,54 +1,29 @@
 <script setup>
-import {
-  ref, computed, watch,
-} from 'vue';
-import { useData } from 'vitepress';
-import Footer from './FooterTemplate.vue';
-import NotFound from './NotFoundTemplate.vue';
-
-const { frontmatter, page } = useData();
-
-const selectedTheme = computed(() => (localStorage.getItem('vitepress-theme-appearance') === 'light' ? 'dark' : 'light'));
-const isDarkMode = ref(selectedTheme.value === 'dark' || false);
-watch(isDarkMode, (() => {
-  localStorage.setItem('vitepress-theme-appearance', selectedTheme.value); const url = new URL(window.location.href);
-  window.location.href = url;
-}));
-
+import { useData } from "vitepress";
+import Footer from "./FooterTemplate.vue";
+import NotFound from "./NotFoundTemplate.vue";
+const { site, frontmatter, page } = useData();
 </script>
 <template>
   <div class="main">
     <nav>
-      <a class="logo" href="/" >
-        Hi, I am Viviana
-        <span role="img" aria-hidden="true">⚡️</span>
+      <a class="logo" href="/">
+        <img src="../../public/logo-viviyanez.png" alt="viviyanez.dev"/>
       </a>
       <div class="menu">
-        <ul>
-          <li>
-            <a :class="{ active: frontmatter.title === 'Projects' }"
+        <a
+          :class="{ active: frontmatter.title === 'Projects' }"
           href="/projects/"
           >/projects</a
-        ></li>
-
-        <li> <a :class="{ active: frontmatter.title === 'Blog' }" href="/blog/"
+        >
+        <a :class="{ active: frontmatter.title === 'Blog' }" href="/blog/"
           >/blog</a
-        ></li>
-        <li> <a href="https://github.com/vivitt" target="_blank">/GitHub</a></li>
+        >
+        <a href="https://github.com/vivitt" target="_blank">/GitHub</a>
 
-       <li> <a href="https://www.linkedin.com/in/viviana-yanez/" target="_blank"
+        <a href="https://www.linkedin.com/in/viviana-yanez/" target="_blank"
           >/LinkedIn</a
         >
-      </li>
-      <li>
-        <label for="mode">switch mode</label>
-        <input
-        v-model="isDarkMode"
-        id="mode"
-        type="checkbox"
-        />
-      </li>
-      </ul>
       </div>
     </nav>
     <div class="content">
